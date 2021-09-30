fb2113e319c804de05cfe894dcb9b17ddb3162e5
Mehmet Cetin <c3t0@Mehmets-MacBook-Air.local>
2021-09-30 15:07:29 +0200

    parallel version with 2 procs

--- MPI_assignment/test.c
+++ MPI_assignment/test.c
@@ -9,19 +9,19 @@ int N = 500;
 int R = 100;
 
 int test(int x) {
-  // Transform to a number beween 0 and 2 * pi.
-  double xd = ((double)x * (2 * M_PI) / N);
+	// Transform to a number beween 0 and 2 * pi.
+	double xd = ((double)x * (2 * M_PI) / N);
 
-  // Do a fixed amount of computation.
-  int n = 25000000;
-  for (int i = 0; i < n; i++) {
-    xd = sin(xd);
-    xd = asin(xd);
-  }
-  xd = sin(xd);
+	// Do a fixed amount of computation.
+	int n = 25000000;
+	for (int i = 0; i < n; i++) {
+		xd = sin(xd);
+		xd = asin(xd);
+	}
+	xd = sin(xd);
 
-  // Test the result.
-  return xd < -0.5;
+	// Test the result.
+	return xd < -0.5;
 }
 
 int test_imbalanced(int x) {
@@ -42,18 +42,18 @@ int test_imbalanced(int x) {
 }
 
 int *allocate_mem(int N) {
-  int *A = calloc(N, sizeof(int));
-  if (!A)
-    exit(0);
-  return A;
+	int *A = calloc(N, sizeof(int));
+	if (!A)
+		exit(0);
+	return A;
 }
 
 void fill_random(int *A, int N) {
-  srand(time(0));
+	srand(time(0));
 
-  for (int i = 0; i < N; i++) {
-    A[i] = (rand() % N);
-  }
+	for (int i = 0; i < N; i++) {
+		A[i] = (rand() % N);
+	}
 }
 
 void fill_ascending(int *A, int N) {
@@ -63,27 +63,95 @@ void fill_ascending(int *A, int N) {
 }
 
 void sequential() {
-  int *arr = allocate_mem(N);
-  int nr_true = 0;
-  fill_random(arr, N);
-  
-  printf("Sequential program has started!\n");
-  clock_t begin = clock();
+	int *arr = init_random_arr();
+	int nr_true = 0;
+	
+	printf("Sequential program has started!\n");
+	clock_t begin = clock();
 
-  for (int i = 0; i < N; i++) {
-    int result = test(arr[i]);
-    if (result) {
-      nr_true++;
-    }
-  }
+	for (int i = 0; i < N; i++) {
+		int result = test(arr[i]);
+		if (result) {
+		nr_true++;
+		}
+	}
 
-  clock_t end = clock();
-  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-  printf("Time spent (seconds) for the sequential version: %f\n", time_spent);
+  	clock_t end = clock();
+  	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  	printf("Time spent (seconds) for the sequential version: %f\n", time_spent);
 
-  printf("Number of trues for N=%d, R=%d: %d\n", N, R, nr_true);
+  	printf("Number of trues for N=%d, R=%d: %d\n", N, R, nr_true);
 }
 
+
+int *init_random_arr() {
+	int *arr = allocate_mem(N);
+	fill_random(arr, N);
+	return arr;
+}
+
+int *init_ascending_arr() {
+	int *arr = allocate_mem(N);
+	fill_ascending(arr, N);
+	return arr;
+}
+/*
+	- To reduce the communication overhead, we will reserve (N / nr_procs) job per processor
+	- Make sure that the program keeps track of how many successful elements (number of trues) all processes have found together, 
+	so that all processes can terminate as fast as possible (When nr_trues >= 100). 
+*/
+void parallel_work(int nr_procs, int proc_id, int jobs_per_proc) {
+	printf("Parallel program for processor %d has started!\n", proc_id);
+	if (proc_id == 0) { 			// Root processca
+		int *arr = init_ascending_arr();
+		int nr_true = 0;
+		fill_random(arr, N);
+
+		MPI_Send(arr, N, MPI_BYTE, 1, 0, MPI_COMM_WORLD);
+		for (int i = 0; i < job_per_proc; i++) {
+			int result = test(arr[i]);
+			if (result) {
+				nr_true++;
+			}
+		}
+  	} 
+	else if (proc_id == 1) {
+		int nr_true = 0;
+    	MPI_Recv(&arr, N, MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		printf("Process 1 received data %d from process 0\n", number);
+		for (int i = job_per_proc; i < N; i++) {
+			int result = test(arr[i]);
+			if (result) {
+				nr_true++;
+			}
+		}
+  	} 	
+	
+  	printf("Number of trues for N=%d, R=%d: %d\n", N, R, nr_true);
+}
+
+void initialize_mpi(int &nr_procs, int &proc_id, int &name_len, char *proc_names) {
+	MPI_Init(&argc, &argv); 						// Initialize MPI env
+	MPI_Comm_size(MPI_COMM_WORLD, &nr_procs); 		// Get number of processors we are gonna use for the job
+    MPI_Comm_rank(MPI_COMM_WORLD, &proc_id); 		// Get rank (id) of processors
+	MPI_Get_processor_name(proc_names, &name_len); 	// Get current processor name
+}
 int main(int argc, char *argv[]) {
-  sequential();
+	int nr_procs = 0;
+	int proc_id = -1;
+	int name_len = 0; 
+	char proc_names[MPI_MAX_PROCESSOR_NAME];
+	initialize_mpi(nr_procs, proc_id, name_len, proc_names);
+
+	clock_t begin = clock();
+
+	int job_per_proc = N / nr_procs;
+	parallel_work(nr_procs, proc_id, job_per_proc);
+	
+	clock_t end = clock();
+  	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  	printf("Time spent (seconds) for the paralell version with %d processors: %f\n", nr_procs, time_spent);
+	
+	MPI_Finalize(); // Finalize MPI env
+  	
 }
