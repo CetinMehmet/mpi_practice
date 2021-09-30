6e7f5eccd8b1f34675d1bac43f5975d09aa6959a
PPP course <ppp2108@fs0.cm.cluster>
2021-09-30 13:54:11 +0200

    Sequential job added

--- MPI_assignment/test.c
+++ MPI_assignment/test.c
@@ -9,75 +9,81 @@ int N = 500;
 int R = 100;
 
 int test(int x) {
-	// Transform to a number beween 0 and 2 * pi.
-	double xd = ((double)x * (2 * M_PI) / N);
+  // Transform to a number beween 0 and 2 * pi.
+  double xd = ((double)x * (2 * M_PI) / N);
 
-	// Do a fixed amount of computation.
-	int n = 25000000;
-	for (int i = 0; i < n; i++) {
-		xd = sin(xd);
-		xd = asin(xd);
-	}
-	xd = sin(xd);
+  // Do a fixed amount of computation.
+  int n = 25000000;
+  for (int i = 0; i < n; i++) {
+    xd = sin(xd);
+    xd = asin(xd);
+  }
+  xd = sin(xd);
 
-	// Test the result.
-	return xd < -0.5;
+  // Test the result.
+  return xd < -0.5;
 }
 
 int test_imbalanced(int x) {
-	// Transform to a number beween 0 and 2 * pi.
-	double xd = ((double)x * (2 * M_PI) / N);
+  // Transform to a number beween 0 and 2 * pi.
+  double xd = ((double)x * (2 * M_PI) / N);
 
-	// Do computation, the amount depends on the value of x.
-	int n = 100000 * x;
+  // Do computation, the amount depends on the value of x.
+  int n = 100000 * x;
 
-	for (int i = 0; i < n; i++) {
-		xd = sin(xd);
-		xd = asin(xd);
-	}
-	xd = sin(xd);
+  for (int i = 0; i < n; i++) {
+    xd = sin(xd);
+    xd = asin(xd);
+  }
+  xd = sin(xd);
 
-	// Test the result.
-	return xd < -0.5;
+  // Test the result.
+  return xd < -0.5;
 }
 
 int *allocate_mem(int N) {
-	int *A = calloc(N, sizeof(int));
-	if (!A)
-		exit(0);
-	return A;
+  int *A = calloc(N, sizeof(int));
+  if (!A)
+    exit(0);
+  return A;
 }
 
 void fill_random(int *A, int N) {
-	srand(time(0));
+  srand(time(0));
 
-	for (int i = 0; i < N; i++) {
-		A[i] = (rand() % N);
-	}
+  for (int i = 0; i < N; i++) {
+    A[i] = (rand() % N);
+  }
 }
 
 void fill_ascending(int *A, int N) {
-	for (int i = 0; i < N; i++) {
-		A[i] = i;
-	}
+  for (int i = 0; i < N; i++) {
+    A[i] = i;
+  }
 }
 
 void sequential() {
-	int* arr = allocate_mem(N);
-	fill_random(arr, N);
+  int *arr = allocate_mem(N);
+  int nr_true = 0;
+  fill_random(arr, N);
+  
+  printf("Sequential program has started!\n");
+  clock_t begin = clock();
 
-	clock_t begin = clock();
+  for (int i = 0; i < N; i++) {
+    int result = test(arr[i]);
+    if (result) {
+      nr_true++;
+    }
+  }
 
-	for (int i = 0; i < N; i++) {
-		int result = test(arr[i]);
-		printf("%d", result);
-	}
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("Time spent (seconds) for the sequential version: %f\n", time_spent);
 
-	clock_t end = clock();
-	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-	printf("Time spent for the sequential version: %f", time_spent);
+  printf("Number of trues for N=%d, R=%d: %d\n", N, R, nr_true);
 }
+
 int main(int argc, char *argv[]) {
-	sequential();
-	return 0;
+  sequential();
 }
