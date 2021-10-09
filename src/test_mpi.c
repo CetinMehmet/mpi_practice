4459d86792d7c32a08dcd34c67529a3818f0f1a8
Mehmet Cetin <c3t0@Mehmets-Air.home>
2021-10-09 14:41:49 +0200

    Test imbalanced init

--- src/test_mpi.c
+++ src/test_mpi.c
@@ -71,20 +71,24 @@ void fill_ascending(int *A, int N) {
   }
 }
 
-void sequential(char *work_type, FILE *fp) {
+/* Work done by 1 processor */
+void sequential(char *fill_type, char *work_type, FILE *fp) {
 	int *arr = allocate_mem(N);
-	if (strcmp(work_type, "asc") == 0) {
+	if (strcmp(fill_type, "asc") == 0) {
 		fill_ascending(arr, N); 	
-	} else if (strcmp(work_type, "rand") == 0) {
+	} else if (strcmp(fill_type, "rand") == 0) {
 		fill_random(arr, N);
 	} else {
 		printf("Wrong filling for the array.\n"); exit(1);
 	}
 	
 	int nr_true = 0;
+	int result = 0;
 	double time = -MPI_Wtime(); // This command helps us measure time. 
 	for (int i = 0; i < N; i++) {
-		int result = test(arr[i]);
+		if (work_type == "imbalanced") result = test(arr[i]);
+		else result = test_imbalanced(arr[i]);
+
 		if (result) {
 			nr_true++;
 			if (nr_true >= 100) break; 
@@ -94,6 +98,98 @@ void sequential(char *work_type, FILE *fp) {
   	time += MPI_Wtime();
   	fprintf(fp, "Time spent for the sequential version: %f\n", time);
 }
+
+
+// Used for imbalanced test.
+void do_job(int job_per_proc, int *sub_arr) {
+	int nr_true = 0;
+	int halt_job = 0;
+	for (int i = 0; i < job_per_proc; i++) {
+
+		// ** Implicit recieving: check if number of trues exceeds 100 in total.
+		int flag = 0;
+		MPI_Status status;
+		MPI_Iprobe(ROOT, TAG_HALT_JOB, MPI_COMM_WORLD, &flag, &status); 
+		while (flag) {
+			MPI_Recv(&halt_job, 1, MPI_INT, ROOT, TAG_HALT_JOB, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // blocking recv parameter
+			if (halt_job) return;
+			MPI_Iprobe(ROOT, TAG_HALT_JOB, MPI_COMM_WORLD, &flag, &status); // check for more updates
+		}
+
+		int result = test_imbalanced(sub_arr[i]);
+		if (result) {
+			nr_true++;
+			int magic_number = 1;
+			MPI_Send(&magic_number, 1, MPI_INT, ROOT, TAG_NR_TRUES, MPI_COMM_WORLD);  
+			if (nr_true >= 100) return;
+		}
+	}
+}
+
+
+/* test_imbalanced is tested with multiple processors */
+void imbalanced_parallel_work(int nr_procs, int proc_id, char* work_type, FILE *fp) {
+	int job_per_proc = (N / (nr_procs));
+	int *sub_arr = allocate_mem(job_per_proc);
+	int *arr = NULL; 
+	int halt_job = 1;
+
+	if (proc_id == ROOT) { 			// Root machine distributes work
+		arr = allocate_mem(N);
+		if (strcmp(work_type, "asc") == 0) {
+			fill_ascending(arr, N); 	
+		} else if (strcmp(work_type, "rand") == 0) {
+			fill_random(arr, N);
+		} else {
+			fprintf(stderr, "Wrong filling for the array.\n"); exit(1);
+		}
+	}
+	// Scatter the random numbers from the root process to all processes in the MPI world
+  	MPI_Scatter(arr, job_per_proc, MPI_INT, sub_arr, job_per_proc, MPI_INT, ROOT, MPI_COMM_WORLD);
+
+	double time = -MPI_Wtime(); // This command helps us measure time. 
+	if (proc_id == ROOT) { 					
+		int total_nr_true = 0; 
+		int i = 0;
+		while (total_nr_true < 100 && i < job_per_proc) {
+			// Computation that the root process does
+			int result = test_imbalanced(sub_arr[i]); i++;
+			if (result) {
+				total_nr_true++;
+				if (total_nr_true >= 100) {
+					for (int id = 1; id < nr_procs; id++) {
+						MPI_Send(&halt_job, 1, MPI_INT, id, TAG_HALT_JOB, MPI_COMM_WORLD);
+					}
+					break; // Break early to not recieve messages sent from other procs
+				}
+			}
+			// Check if there is a message from another process to update the nr_trues.
+			int flag = 0;
+			MPI_Status status;
+			MPI_Iprobe(MPI_ANY_SOURCE, TAG_NR_TRUES, MPI_COMM_WORLD, &flag, &status); // ** Implicit recieving, check for pending message, don't wait for it then it will increase the time
+			// Non-blocking recv
+			while (flag) {
+				int other_true = 0;
+				MPI_Recv(&other_true, 1, MPI_INT, MPI_ANY_SOURCE, TAG_NR_TRUES, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+				total_nr_true++;
+				if (total_nr_true >= 100) { // If we always recieve a message and can't get out of this loop, we return ASAP
+					for (int id = 1; id < nr_procs; id++) {
+						MPI_Send(&halt_job, 1, MPI_INT, id, TAG_HALT_JOB, MPI_COMM_WORLD);
+					}
+					break; // Break early to not recieve messages sent from other procs
+				}
+				MPI_Iprobe(MPI_ANY_SOURCE, TAG_NR_TRUES, MPI_COMM_WORLD, &flag, &status); // check for more updates
+			}
+		}
+	}
+	else { 
+		do_job(job_per_proc, sub_arr);
+	}
+	time += MPI_Wtime();
+	fprintf(fp, "Process %d finished the job in %f seconds\n", proc_id, time);
+}
+
+
 
 
 /*
@@ -138,11 +234,13 @@ double fixed_parallel_work(int nr_procs, int proc_id, char* work_type, FILE *fp)
 
 
 int main(int argc, char *argv[]) {
-	if (argc != 3) {
-		fprintf(stderr, "An argument of asc or rand, and a file name for the output to be written must be given\n"); exit(1);
+	if (argc != 4) {
+		fprintf(stderr, "arg[1] = rand or asc, arg[2] = file_name.txt, arg[3] = imbalanced or fixed\n"); exit(1);
 	} 
 	char *arr_filling = argv[1]; // rand or asc
 	char *file_name = argv[2]; // file_name.txt
+	char *work_type = argv[3]; // imbalanced or fixed
+
 	// Initialize MPI env
 	MPI_Init(&argc, &argv); 						
 	int nr_procs = 0;
@@ -157,14 +255,27 @@ int main(int argc, char *argv[]) {
 		fp = fopen(file_name, "w+"); 
 	}
 
-	if (nr_procs > 1) {
-		double time = fixed_parallel_work(nr_procs, proc_id, arr_filling, fp);
-		if (proc_id == ROOT) fprintf(fp, "Time took to complete %s work with %d processors: %f\n", arr_filling, nr_procs, time);
+	if (work_type == "fixed") {
+		if (nr_procs > 1) {
+			double time = fixed_parallel_work(nr_procs, proc_id, arr_filling, fp);
+			if (proc_id == ROOT) fprintf(fp, "Time took to complete %s work with %d processors: %f\n", arr_filling, nr_procs, time);
+		} 
+		else {
+			sequential(arr_filling, work_type, fp);
+			fprintf(fp, "\n"); 
+		}
+	} 
+	else if (work_type == "imbalanced") {
+		if (nr_procs > 1) {
+			imbalanced_parallel_work(nr_procs, proc_id, arr_filling, fp);
+			// if (proc_id == ROOT) fprintf(fp, "Time took to complete %s work with %d processors: %f\n", arr_filling, nr_procs, time);
+		} 
+		else {
+			sequential(arr_filling, work_type, fp);
+			fprintf(fp, "\n"); 
+		}
 	}
-	else {
-		sequential(arr_filling, fp);
-		fprintf(fp, "\n"); 
-	}
+	
 
 	fclose(fp);
 	
