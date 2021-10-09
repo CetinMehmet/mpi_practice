934c7f489d5124ba5d7636a1b707ea4a143c1f27
PPP course <ppp2108@fs0.cm.cluster>
2021-10-09 14:48:15 +0200

    benchmark timings added, bash jobs added

---
+++ src/run_rand_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for VAR in 1 2 3 4 5
+do
+    prun -np 1 -1 -script $PRUN_ETC/prun-openmpi ./test_mpi rand rand_work_1.txt
+    prun -np 1 -2 -script $PRUN_ETC/prun-openmpi ./test_mpi rand rand_work_2.txt
+    prun -np 1 -4 -script $PRUN_ETC/prun-openmpi ./test_mpi rand rand_work_4.txt
+    prun -np 1 -8 -script $PRUN_ETC/prun-openmpi ./test_mpi rand rand_work_8.txt
+done
